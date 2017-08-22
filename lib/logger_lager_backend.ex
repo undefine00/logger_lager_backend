da3252b9654a9233e43c7bb9e284b9ab2b3a0b29
Tran Hoan <tr.hoan00@gmail.com>
2017-08-22 14:08:35 +0700

    fix logger_lager_backend Elixir 1.5.1

--- lib/logger_lager_backend.ex
+++ lib/logger_lager_backend.ex
@@ -1,25 +1,34 @@
 defmodule LoggerLagerBackend do
+    @behaviour :gen_event
 
-  @moduledoc """
-  A `lager` backend for `Logger`.
-  """
+    @sink :lager_event
+    @truncation_size 4096
 
-  @sink :lager_event
-  @truncation_size 4096
+    def init(__MODULE__) do
+        {:ok, nil}
+    end
 
-  def init(__MODULE__) do
-    {:ok, nil}
-  end
+    def handle_call({:configure, _opts}, state) do
+        {:ok, :ok, state}
+    end
 
-  def handle_call({:configure, _opts}, state) do
-    {:ok, :ok, state}
-  end
+    defp to_lager_level(:warn), do: :warning
+    defp to_lager_level(level), do: level
 
-  defp to_lager_level(:warn), do: :warning
-  defp to_lager_level(level), do: level
+    def handle_event({level, _groupleader, {Logger, message, _timestamp, metadata}}, state) do
+        :lager.dispatch_log(@sink, to_lager_level(level), metadata, '~ts', [message], @truncation_size, :safe)
+        {:ok, state}
+    end
 
-  def handle_event({level, _groupleader, {Logger, message, _timestamp, metadata}}, state) do
-    :lager.dispatch_log(@sink, to_lager_level(level), metadata, '~ts', [message], @truncation_size, :safe)
-    {:ok, state}
-  end
+    def handle_info(_, state) do
+        {:ok, state}
+    end
+
+    def code_change(_old_vsn, state, _extra) do
+        {:ok, state}
+    end
+
+    def terminate(_reason, _state) do
+        :ok
+    end
 end
